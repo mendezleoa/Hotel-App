35f60a413fa367eee10b32a3d63a7e920c4f0656
mendezleoa <leonardoamm1100@gmail.com>
2024-02-29 22:26:52 -0400

    Autentificación y uso de axios

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Register() {
 
     return (
       <>
         <p>Register</p>
+        <Link to='/' >Volve a Home</Link>
       </>
     )
   }
